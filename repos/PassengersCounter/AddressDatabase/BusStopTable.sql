a1eca9c00adb96e1d48275348230af14936e3668
Ilgar Mamishov <MamishovIN@gmail.com>
2019-03-05 16:54:34 +0300

    del

--- repos/PassengersCounter/AddressDatabase/BusStopTable.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[BusStopTable]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [nameBusStop] NVARCHAR(MAX) NOT NULL, 
-    [coordinateBusStop] [sys].[geography] NOT NULL, 
-    [countPassengers] INT NOT NULL
-)
