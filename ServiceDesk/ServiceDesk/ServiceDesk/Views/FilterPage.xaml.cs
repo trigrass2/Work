413756e1d5842f20fc023614ba88e66ce9daf5d6
Ilgar Mamishov <MamishovIN@gmail.com>
2019-07-03 09:26:06 +0300

    +

---
+++ ServiceDesk/ServiceDesk/ServiceDesk/Views/FilterPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ServiceDesk.Models;
+using ServiceDesk.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ServiceDesk.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FilterPage : ContentPage
+	{
+        FilterPageViewModel _filterPageViewModel;
+		public FilterPage (ServiceDesk_TaskListView filter)
+		{
+			InitializeComponent ();
+            _filterPageViewModel = new FilterPageViewModel(filter) { Navigation = this.Navigation };
+            BindingContext = _filterPageViewModel;
+		}
+	}
+}
