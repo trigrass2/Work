6dc469c24c90a7b08971ce1e08abb72bb2f98e26
Ilgar <mamishovin@gmail.com>
2019-07-25 23:41:28 +0300

    cfdfd

---
+++ Vertical/Vertical/Vertical/CustomViews/ImageObjectDataTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vertical.Models;
+using Xamarin.Forms;
+
+namespace Vertical.CustomViews
+{
+    public class ImageObjectDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CheckListDataTemplate { get; set; }
+        public DataTemplate FolderDataTemplate { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var id = (item as SystemObjectModel)?.TypeID;
+
+            switch (id)
+            {  
+                case 2: return CheckListDataTemplate;
+                
+                default: return FolderDataTemplate;
+            }
+        }
+    }
+}
